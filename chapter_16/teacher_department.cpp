f55345bdf52f469870f73dbf884e9e54d191a18c
Daryl <aka.hjd@gmail.com>
2021-08-09 23:38:15 +0800

    chap16

---
+++ chapter_16/teacher_department.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <vector>
+
+class Teacher {
+private:
+    std::string m_name{};
+
+public:
+    Teacher(const std::string &name)
+        : m_name{name}
+    {}
+
+    const std::string &getName() const { return m_name; }
+};
+
+class Department {
+private:
+    // Holding many teachers.
+    std::vector<std::reference_wrapper<const Teacher>> teachers{};
+
+public:
+    void add(const Teacher &teacher) {
+        teachers.push_back(teacher);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Department& dep) {
+        out << "Department: ";
+        // Print all teachers
+        for (const auto& t: dep.teachers) {
+            out << t.get().getName() << ' ';
+        }
+        out << '\n';
+        return out;
+    }
+};
+
+int main() {
+    // Create a teacher outside the scope of the Department
+    Teacher t1{"Bob"};
+    Teacher t2{"Frank"};
+    Teacher t3{"Beth"};
+
+    {
+        // Create a department and add some Teachers to it
+        Department department{}; // create an empty Department
+
+        department.add(t1);
+        department.add(t2);
+        department.add(t3);
+
+        std::cout << department;
+
+    } // department goes out of scope here and is destroyed
+
+    std::cout << t1.getName() << " still exists!\n";
+    std::cout << t2.getName() << " still exists!\n";
+    std::cout << t3.getName() << " still exists!\n";
+
+    return 0;
+}
